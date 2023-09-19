44513c6dca59ecb316deb036d507e675c8bd76e0
arjun3649 <arjun.rakwal3649@gmail.com>
2023-09-19 16:42:31 +0530

    Convert Css to Tailwindcss and Add icons + routes to plus tab in footer

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Dropdown } from "../Dropdown";
 import Contacts from "../Utils/Contacts";
 import { useState } from "react";
 
@@ -5,13 +6,16 @@ export const Home = () => {
   const [contacts] = useState(Contacts);
 
   const mapcontact = contacts.map((prevcontact) => (
-    <div className="Maintext" key={prevcontact.id}>
+    <div className="text-[15px] ml-[30px]" key={prevcontact.id}>
       {prevcontact.Maintext}
       <br />
-      <span className="BottomText"> {prevcontact.Bottomtext}</span> <hr />
+      <span className="text-[10px]"> {prevcontact.Bottomtext}</span> <hr />
     </div>
-
-    
   ));
-  return <div className="contacts">{mapcontact}</div>;
+  return (
+    <>
+      <Dropdown />
+      <div className="overflow-y-scroll ">{mapcontact}</div>
+    </>
+  );
 };
