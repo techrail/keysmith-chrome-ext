9e2228719c0720df066f49b3b1574eda1a29742d
arjun3649 <arjun.rakwal3649@gmail.com>
2023-10-11 13:34:56 +0530

    Implemented Redux to the rest of the pages

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit"
-import contactReducer from "../features/homeSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import contactReducer from "../features/homeSlice";
+
 
 
 export const store = configureStore({
-reducer : contactReducer,
-
-})
+  reducer: contactReducer,
+  
+});
