2019e8e2e78277f9196d1c3af4d7402e75ae354c
jassuwu <20pw14@psgtech.ac.in>
2023-10-21 22:04:27 +0530

    fix: remove .js extension in imports
    Removed the .js extension in the import to be consistent with the standards.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import { BrowserRouter } from "react-router-dom";
