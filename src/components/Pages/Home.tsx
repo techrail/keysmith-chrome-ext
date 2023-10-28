f262cb40d4303ff525a211b6b48d8d14bc5f8658
jassuwu <karthickprnv@gmail.com>
2023-10-28 13:25:54 +0530

    style: :heavy_minus_sign: remove bootstrap, update tailwind
    Removed bootstrap and updated tailwind to the latest version. And removed other unused packages.
    
    This will stop any more conflicts between bootstrap and tailwind styling systems.

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,22 +1,16 @@
 import { RootState } from "../../store/store";
-import { Dropdown } from "../Dropdown";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 
 export const Home = () => {
   const contactList = useSelector((state: RootState) => state.contacts);
 
   const mapcontact = contactList.map((prevcontact) => (
-    <div className="text-[15px] ml-[30px]" key={prevcontact.id}>
-      {prevcontact.mainText}
-
-      <br />
-      <span className="text-[10px]"> {prevcontact.bottomText}</span> <hr />
+    <div className="border-b border-primaryBorder p-2" key={prevcontact.id}>
+      <p className="text-primary">{prevcontact.mainText}</p>
+      <p className="text-xs text-primary"> {prevcontact.bottomText}</p>
     </div>
   ));
   return (
-    <>
-      <Dropdown />
-      <div className="overflow-y-scroll ">{mapcontact}</div>
-    </>
+      <div className="h-full w-full px-2">{mapcontact}</div>
   );
 };
